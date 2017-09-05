2a58650ada51271231d8f4afb943df13f71f90d2
xcodebin <xb865453422@hotmail.com>
2017-09-05 15:55:04 +0800

    00

--- src/views/guide/install.vue
+++ src/views/guide/install.vue
@@ -6,7 +6,6 @@
             <Anchor title="NPM 安装" h2></Anchor>
             <p>推荐使用 npm 来安装，享受生态圈和工具带来的便利，更好地和 webpack 配合使用，当然，我们也推荐使用 ES2015。</p>
             <i-code lang="auto" bg>{{ code.install.install }}</i-code>
-            <p>如果您使用了 NPM 安装，并使用 webpack 作为构建工具，请继续阅读快速上手章节。</p>
         </article>
     </i-article>
 </template>
