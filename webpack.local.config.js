5eed3f1b18580f3c410ac22368c8e1c31c3cd44d
xcodebin <xb865453422@hotmail.com>
2018-05-15 15:54:54 +0800

    1.0

--- webpack.local.config.js
+++ webpack.local.config.js
@@ -9,7 +9,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var fs = require('fs');
 
-config.output.publicPath = path.join('./dist/');
+config.output.publicPath = path.join('./distDoc/');
 config.output.filename = '[name].[hash].js';                 // 带hash值的入口js名称
 config.output.chunkFilename = '[name].[hash].chunk.js';      // 带hash值的路由js名称
 
@@ -35,7 +35,7 @@ config.plugins = (config.plugins || []).concat([
         }
     }),
     new HtmlWebpackPlugin({
-        filename: '../index_prod.html',
+        filename: 'doc_prod.html',
         template: './src/template/index.ejs',
         inject: false
     })
