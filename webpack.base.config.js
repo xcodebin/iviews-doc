b5b3d2730ca2e0214dca12c830f2ff5386b8e516
xcodebin <xb865453422@hotmail.com>
2018-05-15 16:07:11 +0800

    1.0

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -17,7 +17,7 @@ module.exports = {
     },
     // 输出
     output: {
-        path: path.join(__dirname, './dist')
+        path: path.join(__dirname, './distDoc')
     },
     // 加载器
     module: {
